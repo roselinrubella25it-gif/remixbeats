5934d250670b880447095d43bcdbc94277f22064
Roselinrubella <roselinrubella.25it@outlook.com>
2025-11-13 17:54:59 +0530

    Update vite.config.js changes

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
   host: '0.0.0.0',
   port: 5000,
-  allowedHosts: ["https://remixbeats-frontend.onrender.com"]
+  allowedHosts: ["https://remixbeats-backend.onrender.com"]
 }
 }
 )
